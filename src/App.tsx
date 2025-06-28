37c1b331fba313d25b6c5619bf5d54ba2e37a8e0
Mateo <mateozubieta13@gmail.com>
2025-06-27 20:17:10 -0400

    Update dependencies and refactor App component to include routing. Added react-router-dom and updated package-lock.json accordingly. Implemented a landing page route.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
-import { useState } from 'react'
 import './index.css'
-
+import { Route, Routes } from "react-router-dom";
+import { LandingPage } from "./landing/LandingPage";
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-      
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs text-3xl font-bold text-fuchsia-600">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path="/landing" element={<LandingPage />} />
+    </Routes>
   )
 }
 
